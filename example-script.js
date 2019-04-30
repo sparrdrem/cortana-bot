59d4f09af263f2923cedc03111e4dfe659ff80f8
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2019-04-29 21:08:47 -0700

    Create example script

---
+++ example-script.js
@@ -0,0 +1,13 @@
+// Example Script - The skeleton of what a command
+// for Discord BOTs should look like.
+//
+// -- Begin Declaration --
+//
+//case 'ver':
+//    bot.sendMessage({
+//        to: channelID,
+//        message: 'Cortana BOT for Discord. Version 1.00.00.01. Created by SparrDrem, 2019-30-04 10:19:25:00'
+//    })
+//break;
+//
+// -- End Declaration --
