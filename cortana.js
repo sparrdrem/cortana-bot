217d18c2229a47abe1648ece8fd56eeaba96ec39
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2019-04-29 20:17:55 -0700

    Add payload commands
    Accidentally pressed enter during editing session

--- cortana.js
+++ cortana.js
@@ -4,6 +4,34 @@ var auth = require('.\auth.json');
 // Logger configurations
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
-    colorize: true
+        colorize: true
 });
-logger.level = 'debug
+logger.level = 'debug';
+// Initialize Cortana Payload
+var bot = new Discord.Client({
+        token: auth.token,
+        autorun: true
+});
+bot.on('ready', function (evt) {
+        logger.info('Connected');
+        logger.info('Logged in as: ');
+        logger.info(bot.username + ' - ('+ bot.id +')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+        // Cortana will look for '!' in the chat
+        if (message.substring(0, 1) == '!') {
+                var args = message.substring(1).split('');
+                var cmd = args[0];
+                
+                args = args.splice(1);
+                switch(cmd) {
+                        // Display current version
+                    case 'ver':
+                            bot.sendMessage({
+                                    to: channelID,
+                                    message: 'Cortana BOT for Discord. Version 1.00.00.01. Created by SparrDrem, 2019-30-04 10:19:25:00'
+                            })
+                    break;
+                }
+        }
+});
