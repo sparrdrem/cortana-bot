c1ef72a2d0fbff0ec62f48e3c5c1501edb0e4a22
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2019-04-30 19:03:16 -0700

    Change A LOT of stuff to make work

--- cortana.js
+++ cortana.js
@@ -1,44 +1,16 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('.\auth.json');
-// Logger configurations
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-        colorize: true
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
-logger.level = 'debug';
-// Initialize Cortana Payload
-var bot = new Discord.Client({
-        token: auth.token,
-        autorun: true
+
+client.on('message', msg => {
+  if (msg.content === '!ver') {
+    msg.channel.send('Cortana BOT for Discord.');
+    msg.channel.send('Version 1.00.00.01.');
+    msg.channel.send('Created by SparrDrem, 2019-30-04 10:19:25:00');
+  }
 });
-bot.on('ready', function (evt) {
-        logger.info('Connected');
-        logger.info('Logged in as: ');
-        logger.info(bot.username + ' - ('+ bot.id +')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-        // Cortana will look for '!' in the chat
-        if (message.substring(0, 1) == '!') {
-                var args = message.substring(1).split('');
-                var cmd = args[0];
-                
-                args = args.splice(1);
-                switch(cmd) {
-                        // Display current version
-                    case 'ver':
-                            bot.sendMessage({
-                                    to: channelID,
-                                    message: 'Cortana BOT for Discord. Version 1.00.00.01. Created by SparrDrem, 2019-30-04 10:19:25:00'
-                            })
-                    break;
-                    // Hello World payload
-                    case 'test':
-                            bot.sendMessage({
-                                    to: channelID,
-                                    message: 'Hello World'
-                            })
-                    break;
-                }
-        }
-});
+
+client.login('$dummy_token$');
