9e3f0c2eb0688809598c5c1aa3334a68d76b7310
SparrOSDeveloperTeam <SparrOSDeveloperTeam@users.noreply.github.com>
2019-09-04 11:10:51 -0500

    A secret

--- cortana.js
+++ cortana.js
@@ -18,8 +18,11 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content === '!ver') {
     msg.channel.send('Cortana BOT for Discord.');
-    msg.channel.send('Version 1.00.00.01b.');
+    msg.channel.send('Version 1.00.00.01.');
     msg.channel.send('Created by SparrDrem, 2019-30-04 10:19:25:00');
+  }
+  if (msg.content === '!awdbios') {
+    msg.channel.send('```   Award Modular BIOS v6.0, An Energy Star Ally\n   Copyright (C) 1984-2000, Award Software, Inc.\n\nASUS P4T ACPI BIOS Revision 1003\n\nIntel(R) Pentium(R) 4 1759 MHz Processor\nRDRAM Clock : 440 MHz\nMemory Test :  262144K OK\n\nAward Plug and Play BIOS Extension v1.0A\nInitialize Plug and Play Cards...\nPNP Init Completed\n\nTrend ChipAwayVirus(R) On Guard\n\nDetecting Primary Master  ... IBM-DTTA-371010\nDetecting Primary Slave   ... QUANTUM FIREBALL SE4.3A\nDetecting Secondary Master... [Press F4 to skip]_\n\n\n\n\nPress DEL to enter SETUP\n12/29/2000-1858-<<P4T>>-00```');
   }
   if (msg.content === '!test') {
     msg.channel.send(exampleEmbed);
